53cd626568b2895a4dc5ec9b80673196c4496a93
flameking <2711554770@qq.com>
2022-07-20 13:46:48 +0800

    开启邮件服务、发送html邮件

---
+++ src/main/java/com/flameking/community/utils/MailClient.java
@@ -0,0 +1,45 @@
+package com.flameking.community.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+@Slf4j
+public class MailClient {
+  @Autowired
+  private JavaMailSender mailSender;
+
+  @Value("${spring.mail.username}")
+  private String from;
+
+  /**
+   * 发送邮件
+   * @param to        接收人
+   * @param subject   邮件标题
+   * @param content   邮件正文
+   */
+  public void sendMail(String to, String subject, String content){
+    try {
+      MimeMessage message = mailSender.createMimeMessage();         //创建消息，此时为空
+      MimeMessageHelper helper = new MimeMessageHelper(message);    //使用helper设置邮件内容
+      helper.setFrom(from);
+      helper.setTo(to);
+      helper.setSubject(subject);
+      helper.setText(content, true);                          //开启html解析，这样thymeleaf模板发送邮件了
+      mailSender.send(helper.getMimeMessage());                     //发送邮件
+      log.debug("发送邮件成功");
+
+    } catch (MessagingException e) {
+      log.debug("发送邮件失败：{}", e.getMessage());
+    }
+
+  }
+
+}
---
+++ src/test/java/com/flameking/community/utils/MailClientTest.java
@@ -0,0 +1,39 @@
+package com.flameking.community.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@Slf4j
+public class MailClientTest {
+
+  @Autowired
+  MailClient mailClient;
+
+  @Autowired
+  TemplateEngine templateEngine;
+
+  @Test
+  public void testSendTextMail() {
+    mailClient.sendMail("2711554770@qq.com", "test", "test内容");
+  }
+
+  @Test
+  public void testSendHtmlMail(){
+    Context context = new Context();
+
+    context.setVariable("username", "大帅哥");
+
+    String content = templateEngine.process("/mail/mail-template", context);
+    System.out.println(content);
+
+    mailClient.sendMail("2711554770@qq.com", "我才是标题", content);
+  }
+}
